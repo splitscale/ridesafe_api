308da90ec5f9aec61c7a778857a7e15ac72fd842
jeromecabugwason <jeromecabugwason2@protonmail.com>
2023-03-21 23:10:56 +0800

    refactor api

--- lib/api.dart
+++ lib/api.dart
@@ -1,16 +1,21 @@
+import 'package:ridesafe_api/device/device_service_controller.dart';
 import 'package:ridesafe_api/user/user_repository_controller_switcher.dart';
 
 import 'api_endpoints.dart';
 
 class Api {
-  static late ApiEndpoints _config;
+  static late ApiEndpoints _endpoints;
 
   static void initialize() {
-    _config = ApiEndpoints();
+    _endpoints = ApiEndpoints();
   }
 
-  static UserRepositoryControllerSwitcher get userController {
-    return _config.userController;
+  static UserRepositoryControllerSwitcher get user {
+    return _endpoints.userController;
+  }
+
+  static DeviceServiceController get bluetooth {
+    return _endpoints.deviceController;
   }
 }
 
