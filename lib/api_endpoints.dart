502a5f1e40a350e03dc55975a849d00542ba8509
jeromecabugwason <jeromecabugwason2@protonmail.com>
2023-03-21 23:00:40 +0800

    add api endpoints

--- lib/api_endpoints.dart
+++ lib/api_endpoints.dart
@@ -1,11 +1,20 @@
+import 'package:ridesafe_api/device/device_service_controller.dart';
 import 'package:ridesafe_api/user/user_repository_controller_switcher.dart';
+import 'package:ridesafe_bluetooth_impl/service/bluetooth_service.dart';
 
 class ApiEndpoints {
   final UserRepositoryControllerSwitcher _userRepositorySwitcher;
+  final DeviceServiceController _serviceController;
 
-  ApiEndpoints() : _userRepositorySwitcher = UserRepositoryControllerSwitcher();
+  ApiEndpoints()
+      : _userRepositorySwitcher = UserRepositoryControllerSwitcher(),
+        _serviceController = DeviceServiceController(BluetoothService.service);
 
   UserRepositoryControllerSwitcher get userController {
     return _userRepositorySwitcher;
+  }
+
+  DeviceServiceController get deviceController {
+    return _serviceController;
   }
 }
