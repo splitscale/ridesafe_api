209c3c9662ad329ab4e498f22ce9463510510bd5
jeromecabugwason <jeromecabugwason2@protonmail.com>
2023-03-23 00:59:55 +0800

    add permission type export

--- lib/permission/permission_service_controller.dart
+++ lib/permission/permission_service_controller.dart
@@ -2,6 +2,9 @@ import 'package:ridesafe_core/permission/request/permission_request_interactor.d
 import 'package:ridesafe_core/permission/state/permission_state_interactor.dart';
 import 'package:ridesafe_permission_impl/types/bluetooth_permissions.dart';
 
+export 'package:ridesafe_permission_impl/types/bluetooth_permissions.dart'
+    show BluetoothPermissionMap, BluetoothPermission;
+
 class PermissionServiceController {
   final PermissionRequestInteractor<BluetoothPermissionMap> _requestInteractor;
   final PermissionStateInteractor<BluetoothPermission> _stateInteractor;
