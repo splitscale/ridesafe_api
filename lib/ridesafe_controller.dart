7332b9a3ef3cf5781c15bc5e4504099e20c14fea
jeromecabugwason <jeromecabugwason2@protonmail.com>
2023-03-30 17:57:58 +0800

    add proper initializer
    !! breaking change in ridesafe init flow

--- lib/ridesafe_controller.dart
+++ lib/ridesafe_controller.dart
@@ -6,10 +6,10 @@ import 'package:ridesafe_core/connected_device/connected_device.dart';
 import 'api_endpoints.dart';
 import 'device/connected_device_service_controller.dart';
 
-class Ridesafe {
+class RidesafeController {
   final ApiEndpoints _endpoints;
 
-  Ridesafe(this._endpoints);
+  RidesafeController(this._endpoints);
 
   UserRepositoryControllerSwitcher get user {
     return _endpoints.userController;
@@ -29,24 +29,3 @@ class Ridesafe {
     return _endpoints.getConnectedDeviceController(connectedDevice);
   }
 }
-
-// usage
-
-// main() {
-//   // initialize the controller
-//   Api.initialize();
-// }
-
-// server() {
-//   final UserRepositoryController db = Api.userController.server;
-
-//   final User some = db.create('username');
-//   final User finds = db.getById('someID');
-// }
-
-// local() {
-//   final UserRepositoryController db = Api.userController.local;
-
-//   final User some = db.create('username');
-//   final User finds = db.getById('someID');
-// }
