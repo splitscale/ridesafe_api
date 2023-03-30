b565efd74a266bc61e70eadf4ecd5724594c65bf
jeromecabugwason <jeromecabugwason2@protonmail.com>
2023-03-30 18:50:05 +0800

    refactor init method

--- lib/ridesafe.dart
+++ lib/ridesafe.dart
@@ -1,21 +1,20 @@
 import 'package:ridesafe_api/api_endpoints.dart';
-
 import 'ridesafe_controller.dart';
 
 class Ridesafe {
-  static late RidesafeController _binder;
+  static RidesafeController? _binder;
 
   static Future<void> initialize() async {
-    _binder = RidesafeController(ApiEndpoints());
-
-    await Future.delayed(const Duration(seconds: 1));
+    do {
+      _binder = RidesafeController(ApiEndpoints());
+      await Future.delayed(const Duration(seconds: 1));
+    } while (_binder == null);
   }
 
   static RidesafeController get controller {
-    try {
-      return _binder;
-    } catch (e) {
+    if (_binder == null) {
       throw Exception('RidesafeControllers is not initialized');
     }
+    return _binder!;
   }
 }
